690773806b65426de8372bc2d9edd423805953de
Ezra Kirsh <ezkirsh@gmail.com>
2023-03-30 17:04:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+VANTA.FOG({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x780000,
+    midtoneColor: 0xd24646,
+    lowlightColor: 0x4d0b0b,
+    baseColor: 0xd49696,
+    blurFactor: 0.51
+})
